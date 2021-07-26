3a70046682762a4a4e727687e709378e8246dc3a
majidlowe <75464070+majidlowe@users.noreply.github.com>
2021-07-26 17:16:11 -0500

    Initial commit

---
+++ src/main/java/com/example/virtualcardealership/VirtualCarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualcardealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCarDealershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualCarDealershipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/virtualcardealership/VirtualCarDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualcardealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCarDealershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
